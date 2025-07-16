4e1218067ffcfedd2a61caac037e545556f43051
luis.hk <kul@hss.edu>
2025-07-16 16:02:45 -0400

    updated google api

--- src/api/google.ts
+++ src/api/google.ts
@@ -14,22 +14,19 @@ export const getGoogleImages = async (
   }
 
   if (!GOOGLE_KEY || !GOOGLE_URL || !GOOGLE_CX) {
-    console.log('GOOGLE_KEY: ', GOOGLE_KEY);
-    console.log('GOOGLE_URL: ', GOOGLE_URL);
-    console.log('GOOGLE_CX: ', GOOGLE_CX);
     return undefined;
   }
 
   try {
     const config = {
-      url: 'https://www.googleapis.com/customsearch/v1',
+      url: GOOGLE_URL,
       method: 'get',
       params: {
         q: query,
         searchType: 'image',
         // rights: 'cc_publicdomain|cc_attribute',
-        key: 'AIzaSyBvNdJAUUlESzp30sJKRHwamimmx72xYsg',
-        cx: '96420aea73055446b',
+        key: GOOGLE_KEY,
+        cx: GOOGLE_CX,
       },
     };
     const photoUrls: googleResponseAPIType = await axios(config);
