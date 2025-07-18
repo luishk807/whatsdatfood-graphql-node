7f8e09418c92468420c4499cb8e41ef79092c2a1
luis.hk <kul@hss.edu>
2025-07-18 13:23:40 -0400

    remove rebundant files

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,13 +1,7 @@
 import { Router } from 'express';
-import openaiRouter from 'routes/open-ai.route';
-import restauntRouter from 'routes/restaurant.route';
-import userRouter from 'routes/users.route';
 import restaurantMenuItemRoute from 'routes/resturantItems.route';
 const router = Router();
 
-router.use('/open-ai', openaiRouter);
-router.use('/restaurants', restauntRouter);
-router.use('/users', userRouter);
 router.use('/restaurant-menu-items', restaurantMenuItemRoute);
 
 export default router;
--- src/routes/resturantItems.route.ts
+++ src/routes/resturantItems.route.ts
@@ -1,11 +1,7 @@
 import express from 'express';
-import {
-  createRetaurantMenuItems,
-  getAllRestaurantMenuItems,
-} from 'controllers/restaurantMenuItems.controller';
+import { getAllRestaurantMenuItems } from 'controllers/restaurantMenuItems.controller';
 const router = express.Router();
 
 router.get('/', getAllRestaurantMenuItems);
-router.post('/', createRetaurantMenuItems);
 
 export default router;
